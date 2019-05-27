fca92605c06b61d49e8e1fc17e98f9a941cd1cfc
Murilo Felpeto <murilofelpeto@gmail.com>
2019-05-26 21:33:00 -0300

    git initializr

---
+++ rest-with-springboot-udemy/src/main/java/br/com/murilo/restwithspringbootudemy/RestWithSpringbootUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.murilo.restwithspringbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringbootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringbootUdemyApplication.class, args);
+	}
+
+}
