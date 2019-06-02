18c7e7edba5e16b0516cca349a4e4f4c4a08d629
Murilo Felpeto <murilofelpeto@gmail.com>
2019-06-02 03:35:18 -0300

    Content negotiation

---
+++ 09 - content negotiation/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `person` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address` varchar(100) NOT NULL,
+  `first_name` varchar(80) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+)
