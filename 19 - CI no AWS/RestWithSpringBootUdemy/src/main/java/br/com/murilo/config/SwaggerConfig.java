11101c56dfc9a9819c66a6c08823e253c14401d0
Murilo Felpeto <murilofelpeto@gmail.com>
2019-08-09 16:14:23 -0300

    CI com Travis e AWS

---
+++ 19 - CI no AWS/RestWithSpringBootUdemy/src/main/java/br/com/murilo/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package br.com.murilo.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.murilo")).paths(PathSelectors.any()).build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("RestFul API with spring boot 2.1.4 + Travis-CI + AWS + Docker", "Aulas do curso: do 0 a nuvem com springboot e docker",
+				"v1", "http://google.com.br",
+				new Contact("Murilo Felpeto", "http://murilofelpeto.com", "murilofelpeto@gmail.com"), "API License",
+				"License URL", Collections.emptyList());
+	}
+}
---
+++ 19 - CI no AWS/RestWithSpringBootUdemy/src/main/java/br/com/murilo/repository/PersonRepository.java
@@ -0,0 +1,21 @@
+package br.com.murilo.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.murilo.data.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+	@Modifying
+	@Query("UPDATE Person p SET p.enabled = false WHERE p.id=:id")
+	void disablePerson(@Param("id") Long id);
+	
+	Page<Person> findByFirstNameContaining(@Param("firstName")String firstName, Pageable pageable);
+}
